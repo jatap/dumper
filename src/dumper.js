9baa85b4a59f0979b95b783cad396649500c5698
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2016-04-26 21:21:06 +0100

    Add docs and travis support.

--- src/dumper.js
+++ src/dumper.js
@@ -6,7 +6,13 @@ Object.defineProperty(exports, "__esModule", {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; }; /**
+                                                                                                                                                                                                                                                   * Dumper module.
+                                                                                                                                                                                                                                                   * @module dumper
+                                                                                                                                                                                                                                                   * @todo Refactor module to OOP.
+                                                                                                                                                                                                                                                   * @author Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
+                                                                                                                                                                                                                                                   * @license MIT
+                                                                                                                                                                                                                                                   */
 
 exports.dumper = dumper;
 
@@ -26,9 +32,38 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * @function separator
+ * @param {string} header - Header to show.
+ * @param {string} char - Char to repeat.
+ * @param {string} color - Color.
+ * @returns {String} Separator line.
+ * @version 0.0.1
+ * @since 0.0.1
+ * @see debug
+ * @description Add a separator line.
+ * @summary Separator line.
+ */
 function separator(header, marker, color) {
+  /**
+   * Total number of markers to print.
+   * @type {number}
+   * @static
+   */
   var total = 78 - header.length;
+
+  /**
+   * Content of the line to print.
+   * @type {string}
+   * @static
+   */
   var data = '';
+
+  /**
+   * Number or prefix markers before print header.
+   * @type {number}
+   * @static
+   */
   var prefixSpace = 3;
 
   (0, _lodash2.default)(total).forEach(function (element, index) {
@@ -41,14 +76,45 @@ function separator(header, marker, color) {
   return _chalk2.default[color](data);
 }
 
+/**
+ * @function debugFunction
+ * @param {Function} ob - Function to show.
+ * @returns {String} Functiom info.
+ * @version 0.0.1
+ * @since 0.0.1
+ * @see debug
+ * @description Show function information.
+ * @summary Function info.
+ */
 function debugFunction(obj) {
   return obj.toString();
 }
 
+/**
+ * @function debugInspect
+ * @param {*} ob - Object to show.
+ * @returns {String} Object info.
+ * @version 0.0.1
+ * @since 0.0.1
+ * @see debug
+ * @description Show object information.
+ * @summary Object info.
+ */
 function debugInspect(obj) {
   return util.inspect(obj, { depth: null });
 }
 
+/**
+ * @function debug
+ * @param {*} ob - Function or object to show.
+ * @returns {Array} Function/Object info.
+ * @version 0.0.1
+ * @since 0.0.1
+ * @see debugFunction
+ * @see debugInspect
+ * @description Show function/object information.
+ * @summary Function/Object info.
+ */
 function debug(obj) {
   var header = void 0,
       content = void 0;
@@ -89,6 +155,7 @@ function debug(obj) {
   return [header, content];
 }
 
+/** Var dump requested argument. */
 function dumper(obj) {
   console.log(separator("DEBUG - Init", '*', 'green'));
 
